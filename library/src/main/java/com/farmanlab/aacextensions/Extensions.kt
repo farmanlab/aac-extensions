3ec38154bcb71fbcb44704fd39bd016964c4837d
farmanlab <farmlanlabdev@gmail.com>
2019-03-30 21:29:31 +0900

    [Modify] livedata extensions.

--- library/src/main/java/com/farmanlab/aacextensions/Extensions.kt
+++ library/src/main/java/com/farmanlab/aacextensions/Extensions.kt
@@ -14,13 +14,9 @@ inline fun <reified VM : ViewModel> FragmentActivity.getViewModel(crossinline fa
 
 // region LiveData
 
-fun <X, Y> LiveData<X>.map(body: (X) -> Y): LiveData<Y> {
-    return Transformations.map(this, body)
-}
+fun <X, Y> LiveData<X>.map(body: (X) -> Y): LiveData<Y> = Transformations.map(this, body)
 
-fun <X, Y> LiveData<X>.switchMap(body: (X) -> LiveData<Y>): LiveData<Y> {
-    return Transformations.switchMap(this, body)
-}
+fun <X, Y> LiveData<X>.switchMap(body: (X) -> LiveData<Y>): LiveData<Y> = Transformations.switchMap(this, body)
 
 fun <T> MutableLiveData<T>.setValueIfNew(newValue: T) {
     if (this.value != newValue) value = newValue
